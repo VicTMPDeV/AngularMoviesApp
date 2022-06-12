363f77345d03fa4103a003e3837a7f7c167330df
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-06-12 17:53:04 +0200

    starting detail-movie UI behavior

--- src/app/pages/movies/detail-movie/detail-movie.component.ts
+++ src/app/pages/movies/detail-movie/detail-movie.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-detail-movie',
   templateUrl: './detail-movie.component.html',
   styleUrls: ['./detail-movie.component.scss']
 })
-export class DetailMovieComponent implements OnInit {
+export class DetailMovieComponent implements OnInit, OnDestroy {
+
+  isActive: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isActive = true;
+    console.log(this.isActive);
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
+    console.log(this.isActive);
   }
 
 }
