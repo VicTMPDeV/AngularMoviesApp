b59b7baabab6ac2586ad9aacfd7c3d3a8fabd0a9
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-08-03 16:52:10 +0200

    movies list css layout finished

--- src/app/pages/movies/pages/detail-movie/detail-movie.module.ts
+++ src/app/pages/movies/pages/detail-movie/detail-movie.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { DetailMovieComponent } from '@pages/movies/pages/detail-movie/detail-movie.component';
+import { MatIconModule } from '@angular/material/icon';
 import { DetailMovieRoutingModule } from '@pages/movies/pages/detail-movie/detail-movie-routing.module';
+import { DetailMovieComponent } from '@pages/movies/pages/detail-movie/detail-movie.component';
+import { SpinnerModule } from '../../../../components/spinner/spinner.module';
 
 
 @NgModule({
@@ -14,9 +14,9 @@ import { DetailMovieRoutingModule } from '@pages/movies/pages/detail-movie/detai
   imports: [
     CommonModule,
     DetailMovieRoutingModule,
-    MatProgressSpinnerModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    SpinnerModule
   ]
 })
 export class DetailMovieModule { }
